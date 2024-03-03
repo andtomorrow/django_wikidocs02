1f1cda02acd5da5e1eb9216d85ec6e8fb0778a3d
andtomorrow <taegyun04@gmail.com>
2024-03-03 10:48:26 +0900

    basic work

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.subject
+
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.http import HttpResponseNotAllowed
+from .models import Article
+from .forms import ArticleForm
 
-# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-created_at')
+    context = {'articles': articles}
+    return render(request, 'articles/index.html', context)
+
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {'article': article}
+    return render(request, 'articles/detail.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.created_at = timezone.now()
+            article.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    context = {'form': form}
+    return render(request, 'articles/create.html', context)
